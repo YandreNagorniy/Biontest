8ef0ab90bb0163f0a7bca6f1413ced2dfd3c3a6b
Valera Yandre <yandre00111@gmail.com>
2019-01-20 22:00:04 +0300

    add getter and setter in MethodsModel

--- app/src/main/java/com/example/yandre/biontest/database/model/MethodsModel.java
+++ app/src/main/java/com/example/yandre/biontest/database/model/MethodsModel.java
@@ -8,14 +8,46 @@ import android.arch.persistence.room.PrimaryKey;
 @Entity
 public class MethodsModel {
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Method1Model getK2O() {
+        return K2O;
+    }
+
+    public void setK2O(Method1Model k2O) {
+        K2O = k2O;
+    }
+
+    public Method1Model getP2O5() {
+        return P2O5;
+    }
+
+    public void setP2O5(Method1Model p2O5) {
+        P2O5 = p2O5;
+    }
+
+    public Method2Model getN() {
+        return N;
+    }
+
+    public void setN(Method2Model n) {
+        N = n;
+    }
+
     @PrimaryKey(autoGenerate = true)
-    public long id;
+    private long id;
     @Embedded(prefix = "K2O")
-    public Method1Model K2O;
+    private Method1Model K2O;
     @Embedded(prefix = "P2O5")
-    public Method1Model P2O5;
+    private Method1Model P2O5;
     @Embedded(prefix = "N")
-    public Method2Model N;
+    private Method2Model N;
 
     public MethodsModel(long id, Method1Model K2O, Method1Model P2O5, Method2Model N) {
         this.id = id;
